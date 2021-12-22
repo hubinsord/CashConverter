6e154d80c9354ea014268ee60b132e88f853a8f2
hubinsord <hubinsord@gmail.com>
2021-12-22 02:34:22 +0100

    feature: introduced dependency injection tool - hilt

---
+++ app/src/main/java/com/thedroidsonroids/cashconverter/data/model/Table.kt
@@ -0,0 +1,9 @@
+package com.thedroidsonroids.cashconverter.data.model
+
+data class Table(
+    val table: String,
+    val no: String,
+    val tradingDate: String,
+    val effectiveDate: String,
+//    val rates: List<Rate>,
+)
